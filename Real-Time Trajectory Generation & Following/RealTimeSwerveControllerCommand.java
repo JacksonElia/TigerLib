7a0ea28a65d64f281e5d84f0d9ab88d709e2b33c
Jack <85963782+JacksonElia@users.noreply.github.com>
2023-02-07 12:06:25 -0500

    Update RealTimeSwerveControllerCommand.java

--- Real-Time Trajectory Generation & Following/RealTimeSwerveControllerCommand.java
+++ Real-Time Trajectory Generation & Following/RealTimeSwerveControllerCommand.java
@@ -1,6 +1,5 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
+// This command is almost identical to the WPILib SwerveControllerCommand
+// The key difference is that this command can be canceled mid-trajectory
 
 package frc.robot.commands.autonomous;
 
