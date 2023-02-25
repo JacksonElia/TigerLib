80c9f3f000dfa67d60e57d966b381a8bfa3b7252
Jack <85963782+JacksonElia@users.noreply.github.com>
2023-02-24 21:50:16 -0500

    Changed to use pathplanner trajectories

--- Real-Time Trajectory Generation & Following/FollowRealTimeTrajectory.java
+++ Real-Time Trajectory Generation & Following/FollowRealTimeTrajectory.java
@@ -1,120 +1,122 @@
 // JackLib 2023
+// For this code to work, PathPlannerLib needs to be installed through Gradle
+// https://3015rangerrobotics.github.io/pathplannerlib/PathplannerLib.json
 
-package frc.robot.commands.autonomous;
+package frc.robot.commands;
+
+import java.util.function.BooleanSupplier;
+
+import com.pathplanner.lib.PathConstraints;
+import com.pathplanner.lib.PathPlanner;
+import com.pathplanner.lib.PathPlannerTrajectory;
+import com.pathplanner.lib.PathPoint;
 
 import edu.wpi.first.math.controller.PIDController;
-import edu.wpi.first.math.controller.ProfiledPIDController;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
-import edu.wpi.first.math.trajectory.Trajectory;
-import edu.wpi.first.math.trajectory.TrajectoryConfig;
-import edu.wpi.first.math.trajectory.TrajectoryGenerator;
-import edu.wpi.first.math.trajectory.TrapezoidProfile;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.Constants.DriveConstants;
 import frc.robot.Constants.TrajectoryConstants;
-import frc.robot.subsystems.DriveSubsystem;
-import java.util.List;
-import java.util.function.BooleanSupplier;
+import frc.robot.subsystems.drive.DriveSubsystemImpl;
 
 public class FollowRealTimeTrajectory extends CommandBase {
 
-  // To get the x and y positions of where you want to go, you might have to use a subsystem
-  private final DriveSubsystem driveSubsystem;
-  private final BooleanSupplier whileHeldButtonBooleanSupplier;
-
-  public FollowRealTimeTrajectory(DriveSubsystem driveSubsystem, BooleanSupplier whileHeldButtonBooleanSupplier) {
+  private final DriveSubsystemImpl driveSubsystem;
+  private final boolean resetOdometryToTrajectoryStart;
+  private final BooleanSupplier isFinished;
+  private RealTimePPSwerveControllerCommand followPathPlannerTrajectoryCommand;
+  
+  /**
+   * Follows the specified PathPlanner trajectory.
+   * @param driveSubsystem The subsystem for the swerve drive.
+   * @param trajectoryName The name of the PathPlanner path file. It should not include the filepath or 
+   * .path extension.
+   * @param resetOdometryToTrajectoryStart Set as true if you want the odometry of the robot to be set to the
+   * start of the trajectory.
+   */
+  public FollowRealTimeTrajectory(DriveSubsystemImpl driveSubsystem, boolean resetOdometryToTrajectoryStart, BooleanSupplier isFinished) {
     this.driveSubsystem = driveSubsystem;
-    this.whileHeldButtonBooleanSupplier = whileHeldButtonBooleanSupplier;
-    addRequirements(this.driveSubsystem);
+    this.isFinished = isFinished;
+    // addRequirements(driveSubsystem); // TODO See if you need to add this back
+    this.resetOdometryToTrajectoryStart = resetOdometryToTrajectoryStart;
   }
 
+  // Called when the command is initially scheduled.
   @Override
   public void initialize() {
-
     /* EDIT CODE BELOW HERE */
 
     // X and Y should be in meters
-    // You might have to switch the x and y values and make them negative or positive
     // To get these 3 values, you should use the odometry or poseEstimator
-    double startX = 0;
-    double startY = 0;
-    Rotation2d startRotation = driveSubsystem.odometry.getPoseMeters().getRotation();
-    Pose2d start = new Pose2d(startX, startY, startRotation);
-
+    double startX = driveSubsystem.getPose().getX();
+    double startY = driveSubsystem.getPose().getY();
+    Rotation2d startRotation = driveSubsystem.getPose().getRotation();
+    Translation2d start = new Translation2d(startX, startY);
     // These values should be field relative, if they are robot relative add them to the start values
-    double endX = 1;
-    double endY = 0;
-    Rotation2d endRotation = driveSubsystem.odometry.getPoseMeters().getRotation();
-    Pose2d end = new Pose2d(endX, endY, endRotation);
-
-    // If you want any middle waypoints in the trajectory, add them here
-    List<Translation2d> middleWaypoints = List.of();
-
-    // You should have constans or everything below here
-    double driveMaxSpeedMetersPerSecond = TrajectoryConstants.autoMaxVelocity;
-    double driveMaxAccelerationMetersPerSecond = TrajectoryConstants.autoMaxAcceleration;
-    double turnMaxAngularSpeedRadiansPerSecond = TrajectoryConstants.maxAngularSpeedRadiansPerSecond;
-    double turnMaxAngularSpeedRadiansPerSecondSquared = TrajectoryConstants.maxAngularSpeedRadiansPerSecondSquared;
+    double endX = driveSubsystem.getPose().getX() + 2;
+    double endY = driveSubsystem.getPose().getY();
+    Rotation2d endRotation = Rotation2d.fromDegrees(45);
+    Translation2d end = new Translation2d(endX, endY);
     
     // Your probably only want to edit the P values
     PIDController xController = new PIDController(TrajectoryConstants.xControllerP, 0, 0);
     PIDController yController = new PIDController(TrajectoryConstants.yControllerP, 0, 0);
-    ProfiledPIDController thetaController = new ProfiledPIDController(
-      TrajectoryConstants.thetaProfiledControllerP, 0, 0,
-      new TrapezoidProfile.Constraints(turnMaxAngularSpeedRadiansPerSecond, turnMaxAngularSpeedRadiansPerSecondSquared)
-    );
+    PIDController thetaController = new PIDController(TrajectoryConstants.thetaControllerP, 0, 0);
 
     SwerveDriveKinematics kinematics = DriveConstants.driveKinematics;
+    
+    // Makes a trajectory that factors in holonomic rotation
+    PathPlannerTrajectory trajectoryToFollow = PathPlanner.generatePath(
+      new PathConstraints(TrajectoryConstants.autoMaxVelocity, TrajectoryConstants.autoMaxAcceleration),
+      // position, heading (direction of travel)
+      new PathPoint(start, startRotation, Rotation2d.fromDegrees(0)),
+      // If you want any middle waypoints, add more pathpoints here
+      new PathPoint(end, endRotation, Rotation2d.fromDegrees(0))
+    );                                               
 
     // IMPORTANT: Make sure your driveSubsystem has the methods getPose and setModuleStates
 
     /* EDIT CODE ABOVE HERE (ONLY TOUCH THE REST OF THE CODE IF YOU KNOW WHAT YOU'RE DOING) */
 
-    TrajectoryConfig config = new TrajectoryConfig(
-      driveMaxSpeedMetersPerSecond,
-      driveMaxAccelerationMetersPerSecond)
-      .setKinematics(DriveConstants.driveKinematics)
-      .setStartVelocity(0)
-      .setEndVelocity(0);
-
-    Trajectory trajectory = TrajectoryGenerator.generateTrajectory(
-      start,
-      middleWaypoints,
-      end,
-      config
-    );
-
+    // Makes it so wheels don't have to turn more than 90 degrees
     thetaController.enableContinuousInput(-Math.PI, Math.PI);
+    
+    if (resetOdometryToTrajectoryStart) {
+      driveSubsystem.resetPoseEstimator(trajectoryToFollow.getInitialPose());
+    }
 
-    driveSubsystem.resetOdometry(trajectory.getInitialPose());
-
-    new RealTimeSwerveControllerCommand(
-      trajectory,
+    // Create a PPSwerveControllerCommand. This is almost identical to WPILib's SwerveControllerCommand, but it uses the holonomic rotation from the PathPlannerTrajectory to control the robot's rotation.
+    followPathPlannerTrajectoryCommand = new RealTimePPSwerveControllerCommand(
+      trajectoryToFollow,
       driveSubsystem::getPose, // Functional interface to feed supplier
       kinematics,
-      // Position controllers
       xController,
       yController,
       thetaController,
       driveSubsystem::setModuleStates,
-      whileHeldButtonBooleanSupplier,
+      false,
+      isFinished,
+      new Pose2d(),
       driveSubsystem
-    ).schedule();
+    );
+    
+    followPathPlannerTrajectoryCommand.schedule();
 
   }
 
+  // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {}
 
+  // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {}
 
+  // Returns true when the command should end.
   @Override
   public boolean isFinished() {
     return false;
   }
-
 }
